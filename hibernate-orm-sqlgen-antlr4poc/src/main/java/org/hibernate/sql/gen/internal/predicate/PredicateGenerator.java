70d219dfbb85dafc42d306943f724f4ee69ed42c
John O'Hara <johara@redhat.com>
2015-08-28 17:10:42 +0100

    Added class metadata to api

--- hibernate-orm-sqlgen-antlr4poc/src/main/java/org/hibernate/sql/gen/internal/predicate/PredicateGenerator.java
+++ hibernate-orm-sqlgen-antlr4poc/src/main/java/org/hibernate/sql/gen/internal/predicate/PredicateGenerator.java
@@ -8,9 +8,9 @@ import org.hibernate.sqm.query.predicate.RelationalPredicate;
  * Created by johara on 27/08/15.
  */
 public class PredicateGenerator {
-	public static String generatePredicateClause(Predicate predicate) {
+	public String generatePredicateClause(Predicate predicate) {
 		if(predicate instanceof RelationalPredicate ){
-			return RelationalPredicateSqlGenerator.generateSql(predicate);
+			return new RelationalPredicateSqlGenerator().interpret((RelationalPredicate)predicate);
 		}
 		else{
 			throw new PredicateGeneratorNotImplementedException();
--- hibernate-orm-sqlgen-antlr4poc/src/test/java/org/hibernate/sql/gen/SelectSqlGeneration.java
+++ hibernate-orm-sqlgen-antlr4poc/src/test/java/org/hibernate/sql/gen/SelectSqlGeneration.java
@@ -1,5 +1,12 @@
 package org.hibernate.sql.gen;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.query.parser.ConsumerContext;
 import org.hibernate.query.parser.SemanticQueryInterpreter;
 import org.hibernate.sql.gen.sqm.ConsumerContextTestingImpl;
 import org.hibernate.sqm.query.SelectStatement;
@@ -15,13 +22,45 @@ public class SelectSqlGeneration {
 	@Test
 	public void simpleSelectTest() {
 		final String query = "from DTO d where d.id = 1";
+		final ConsumerContext consumerContext = new ConsumerContextTestingImpl();
 		final SelectStatement selectStatement = (SelectStatement) SemanticQueryInterpreter.interpret(
 				query,
-				new ConsumerContextTestingImpl()
+				consumerContext
 		);
 
-		JdbcSelectPlan jdbcSelectPlan = SqmJdbcInterpreter.interpret( selectStatement, null, null );
+		JdbcSelectPlan jdbcSelectPlan = SqmJdbcInterpreter.interpret( selectStatement, null, null, null );
 
 		assertNotNull( jdbcSelectPlan );
 	}
+
+	@Test
+	public void simpleBootstrapTest() {
+		StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+				.configure( "hibernate.cfg.xml" )
+				.build();
+
+		Metadata metadata = new MetadataSources( standardRegistry )
+				.getMetadataBuilder()
+				.applyImplicitNamingStrategy( ImplicitNamingStrategyJpaCompliantImpl.INSTANCE )
+				.build();
+
+		SessionFactory sessionFactory = metadata.getSessionFactoryBuilder()
+				.build();
+//		.applyBeanManager( getBeanManagerFromSomewhere() )
+
+		assertNotNull(sessionFactory);
+
+		final String query = "from Book b where b.id = 1";
+		final ConsumerContext consumerContext = new ConsumerContextTestingImpl();
+		final SelectStatement selectStatement = (SelectStatement) SemanticQueryInterpreter.interpret(
+				query,
+				consumerContext
+		);
+
+		JdbcSelectPlan jdbcSelectPlan = SqmJdbcInterpreter.interpret( selectStatement, null, null, sessionFactory.getAllClassMetadata() );
+
+		assertNotNull( jdbcSelectPlan );
+
+	}
+
 }
