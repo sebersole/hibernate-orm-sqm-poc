dreab8/hibernate-antlr4-poc

Account for EntityReference in select clause

src/main/java/org/hibernate/sql/ast/expression/EntityReference.java
@@ -1,0 +1,38 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.sql.ast.expression;
+
+import org.hibernate.sql.ast.from.ColumnBinding;
+import org.hibernate.sql.gen.SqlTreeWalker;
+import org.hibernate.type.Type;
+
+/**
+ * @author Andrea Boriero
+ */
+public class EntityReference implements Expression {
+	private final Type ormType;
+	private final ColumnBinding[]  columnBindings;
+
+	public EntityReference(Type ormType, ColumnBinding[] columnBindings) {
+		this.ormType = ormType;
+		this.columnBindings = columnBindings;
+	}
+
+	@Override
+	public Type getType() {
+		return ormType;
+	}
+
+	public ColumnBinding[] getColumnBindings() {
+		return columnBindings;
+	}
+
+	@Override
+	public void accept(SqlTreeWalker sqlTreeWalker) {
+		sqlTreeWalker.visitEntityEpression( this );
+	}
+}

src/main/java/org/hibernate/sql/gen/SqlTreeWalker.java
@@ -24,6 +24,7 @@ import org.hibernate.sql.ast.expression.ColumnBindingExpression;
 import org.hibernate.sql.ast.expression.ConcatExpression;
 import org.hibernate.sql.ast.expression.CountFunction;
 import org.hibernate.sql.ast.expression.CountStarFunction;
+import org.hibernate.sql.ast.expression.EntityReference;
 import org.hibernate.sql.ast.expression.Expression;
 import org.hibernate.sql.ast.expression.MaxFunction;
 import org.hibernate.sql.ast.expression.MinFunction;
@@ -203,21 +204,7 @@ public class SqlTreeWalker {
 	// Expressions
 
 	public void visitAttributeReference(AttributeReference attributeReference) {
-		final boolean needsParens = attributeReference.getColumnBindings().length > 1 && currentlyInPredicate;
-		if ( needsParens ) {
-			appendSql( "(" );
-		}
-
-		String separator = "";
-		for ( ColumnBinding columnBinding : attributeReference.getColumnBindings() ) {
-			appendSql( separator );
-			visitColumnBinding( columnBinding );
-			separator = ", ";
-		}
-
-		if ( needsParens ) {
-			appendSql( ")" );
-		}
+		renderColumnBindings( attributeReference.getColumnBindings() );
 	}
 
 	private void visitColumnBinding(ColumnBinding columnBinding) {
@@ -496,4 +483,27 @@ public class SqlTreeWalker {
 	public void visitRelationalPredicate(RelationalPredicate relationalPredicate) {
 		throw new NotYetImplementedException();
 	}
+
+	public void visitEntityEpression(EntityReference entityExpression) {
+		renderColumnBindings( entityExpression.getColumnBindings() );
+	}
+
+	private void renderColumnBindings(ColumnBinding[] columnBindings) {
+		final boolean needsParens = columnBindings.length > 1 && currentlyInPredicate;
+		if ( needsParens ) {
+			appendSql( "(" );
+		}
+
+		String separator = "";
+		for ( ColumnBinding columnBinding : columnBindings ) {
+			appendSql( separator );
+			visitColumnBinding( columnBinding );
+			separator = ", ";
+		}
+
+		if ( needsParens ) {
+			appendSql( ")" );
+		}
+	}
+
 }

sebersole/hibernate-orm-sqm-poc

src/main/java/org/hibernate/sql/ast/expression/EntityReference.java
@@ -1,0 +1,40 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.sql.ast.expression;
+
+import org.hibernate.sql.ast.from.ColumnBinding;
+import org.hibernate.sql.gen.NotYetImplementedException;
+import org.hibernate.sql.gen.Return;
+import org.hibernate.sql.gen.SqlTreeWalker;
+import org.hibernate.type.Type;
+
+/**
+ * @author Andrea Boriero
+ */
+public class EntityReference extends ExpressionAsReturnSupport {
+	private final Type ormType;
+	private final ColumnBinding[]  columnBindings;
+
+	public EntityReference(Type ormType, ColumnBinding[] columnBindings) {
+		this.ormType = ormType;
+		this.columnBindings = columnBindings;
+	}
+
+	@Override
+	public Type getType() {
+		return ormType;
+	}
+
+	public ColumnBinding[] getColumnBindings() {
+		return columnBindings;
+	}
+
+	@Override
+	public void accept(SqlTreeWalker sqlTreeWalker) {
+		sqlTreeWalker.visitEntityExpression( this );
+	}
+}

src/main/java/org/hibernate/sql/gen/SqlTreeWalker.java
@@ -24,6 +24,7 @@ import org.hibernate.sql.ast.expression.ColumnBindingExpression;
 import org.hibernate.sql.ast.expression.ConcatExpression;
 import org.hibernate.sql.ast.expression.CountFunction;
 import org.hibernate.sql.ast.expression.CountStarFunction;
+import org.hibernate.sql.ast.expression.EntityReference;
 import org.hibernate.sql.ast.expression.Expression;
 import org.hibernate.sql.ast.expression.MaxFunction;
 import org.hibernate.sql.ast.expression.MinFunction;
@@ -242,21 +243,7 @@ public class SqlTreeWalker {
 	// Expressions
 
 	public void visitAttributeReference(AttributeReference attributeReference) {
-		final boolean needsParens = attributeReference.getColumnBindings().length > 1 && currentlyInPredicate;
-		if ( needsParens ) {
-			appendSql( "(" );
-		}
-
-		String separator = "";
-		for ( ColumnBinding columnBinding : attributeReference.getColumnBindings() ) {
-			appendSql( separator );
-			visitColumnBinding( columnBinding );
-			separator = ", ";
-		}
-
-		if ( needsParens ) {
-			appendSql( ")" );
-		}
+		renderColumnBindings( attributeReference.getColumnBindings() );
 	}
 
 	private void visitColumnBinding(ColumnBinding columnBinding) {
@@ -607,4 +594,25 @@ public class SqlTreeWalker {
 		relationalPredicate.getRightHandExpression().accept( this );
 	}
 
+	public void visitEntityExpression(EntityReference entityExpression) {
+		renderColumnBindings( entityExpression.getColumnBindings() );
+	}
+
+	private void renderColumnBindings(ColumnBinding[] columnBindings) {
+		final boolean needsParens = columnBindings.length > 1 && currentlyInPredicate;
+		if ( needsParens ) {
+			appendSql( "(" );
+		}
+
+		String separator = "";
+		for ( ColumnBinding columnBinding : columnBindings ) {
+			appendSql( separator );
+			visitColumnBinding( columnBinding );
+			separator = ", ";
+		}
+
+		if ( needsParens ) {
+			appendSql( ")" );
+		}
+	}
 }